386be4f7e7463156ff450b460cdd9445d55b86f6
Farida152005 <farida.elogueil@ucalgary.ca>
2024-11-10 01:08:55 -0700

    made phase card stuff

---
+++ my-app/app/components/PhraseCardFull.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const WordCard = () => {
+    //data (MY SQL PEEPS)
+    const WordData= {
+        word: "Word 1",
+        category: "xyz",
+        definition: "abc",
+        commonuses: ["abc", "xyz"]
+    };
+    <link href="/your-path-to-uicons/css/uicons-[phasecardfull].css" rel="stylesheet"></link>
+    return (
+        <div className="word-page">
+            {/*Save Icon*/}
+            <div className="icon save-icon"></div>
+
+        </div>
+    );
+}//<a href="https://www.flaticon.com/free-icons/save" title="save icons">Save icons created by Freepik - Flaticon</a>

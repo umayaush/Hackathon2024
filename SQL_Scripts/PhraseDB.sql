dd0381bca47176a5953e3c952911ec6f99027e0b
Umaya Hewaarachchi <umayaush@gmail.com>
2024-11-09 22:31:06 -0700

    Cleaned up script and fixed naming consistency

--- SQL_Scripts/PhraseDB.sql
+++ SQL_Scripts/PhraseDB.sql
@@ -4,20 +4,21 @@ USE phrasedb;
 
 -- Create Category table
 CREATE TABLE CATEGORY (
-    category_id         INT AUTO_INCREMENT,
-    CategoryName       VARCHAR(255)        NOT NULL UNIQUE,
-    CategoryDescription VARCHAR(255),
+    category_id         INT                 AUTO_INCREMENT,
+    category_name       VARCHAR(255)        NOT NULL UNIQUE,
+    category_desc       VARCHAR(255),
     
     CONSTRAINT PK_CATEGORY PRIMARY KEY (category_id)
 );
 
 -- Create Phrase table
 CREATE TABLE PHRASE (
-    phrase_id INT AUTO_INCREMENT,
-    category_id INT NOT NULL,
-    phrase VARCHAR(255) NOT NULL UNIQUE,
-    PhraseDescription VARCHAR(255),
-    
+    phrase_id           INT                 AUTO_INCREMENT,
+    category_id         INT                 NOT NULL,
+    phrase              VARCHAR(255)        NOT NULL UNIQUE,
+    phrase_desc         VARCHAR(255),                           -- phrase definition
+    context             VARCHAR(255)        NOT NULL,           -- phrase context
+
     CONSTRAINT PK_PHRASE PRIMARY KEY (phrase_id),
     CONSTRAINT FK_PHRASE_CATEGORY FOREIGN KEY (category_id) 
         REFERENCES CATEGORY(category_id)
@@ -26,30 +27,29 @@ CREATE TABLE PHRASE (
 
 -- Create User table
 CREATE TABLE USER (
-    user_id INT AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    user_password VARCHAR(255) NOT NULL,
+    user_id             INT                 AUTO_INCREMENT,
+    user_email          VARCHAR(255)        NOT NULL UNIQUE,
+    user_password       VARCHAR(255)        NOT NULL,
     
     CONSTRAINT PK_USER PRIMARY KEY (user_id)
 );
 
 -- Create Examples table
-CREATE TABLE EXAMPLES (
-    example_id INT AUTO_INCREMENT,
-    expression_id INT NOT NULL,
-    context VARCHAR(255) NOT NULL,
-    example VARCHAR(255) NOT NULL,
+CREATE TABLE EXAMPLE (
+    example_id          INT                 AUTO_INCREMENT,
+    phrase_id           INT                 NOT NULL,
+    example             VARCHAR(255)        NOT NULL,           -- phrase example
     
-    CONSTRAINT PK_EXAMPLES PRIMARY KEY (example_id),
-    CONSTRAINT FK_EXAMPLES_PHRASE FOREIGN KEY (expression_id) 
+    CONSTRAINT PK_EXAMPLE PRIMARY KEY (example_id),
+    CONSTRAINT FK_EXAMPLE_PHRASE FOREIGN KEY (expression_id) 
         REFERENCES PHRASE(phrase_id)
         ON DELETE CASCADE
 );
 
 -- Create Saved_Phrase table (junction table)
 CREATE TABLE SAVED_PHRASE (
-    user_id INT,
-    expression_id INT,
+    user_id             INT,
+    phrase_id           INT,
     
     CONSTRAINT PK_SAVED_PHRASE PRIMARY KEY (user_id, expression_id),
     CONSTRAINT FK_SAVED_PHRASE_USER FOREIGN KEY (user_id) 
